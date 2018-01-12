76dec52fb6a1dc28e190d66d8219401a81bd754e
ravinder <jajoriaravinder99@gmail.com>
2018-01-12 14:30:11 +0530

    serving 404 done

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ let fs = require('fs');
 const http = require('http');
 const timeStamp = require('./time.js').timeStamp;
 const WebApp = require('./webapp');
-let registered_users = [{userName:'ravinder',name:'Ravinder Jajoria'},{userName:'neeraj',name:'Neeraj Jaiswal'}];
-let comments = fs.readFileSync("./data/comments.json","utf8");
-comments = JSON.parse(comments);
 
 let toS = o=>JSON.stringify(o,null,2);
 
@@ -16,32 +13,6 @@ const isFile = function (fileName) {
     return false;
   }
 };
-
-const showLoginPageLink = function(commentPage,res){
-  let loginPageLink = "<a href=\"/loginPage.html\">login to submit comment</a>";
-  commentPage = commentPage.replace(/COMMENT_FORM/,loginPageLink);
-  res.write(commentPage);
-  res.end();
-}
-
-const showCommentForm = function(commentPage,res){
-  let commentForm = fs.readFileSync("public/templates/commentForm.html","utf8");
-  commentPage = commentPage.replace(/COMMENT_FORM/,commentForm);
-  res.write(commentPage);
-  res.end();
-}
-
-const serveGuestPage = function(req,res){
-    res.setHeader("Content-Type",getContentType("guest-page.html"));
-    fs.readFile("public/templates/guest-page.html",(err,data)=>{
-      if(err) return;
-      if(req.user){
-        showCommentForm(data.toString(),res);
-        return;  
-      }
-      showLoginPageLink(data.toString(),res);
-    });
-}
 
 const serveFile = function (req, res) {
   if(req.url=="/") req.url = "/index.html";
@@ -65,20 +36,6 @@ let logRequest = (req,res)=>{
   console.log(`${req.method} ${req.url}`);
 }
 
-let loadUser = (req,res)=>{
-  let sessionid = req.cookies.sessionid;
-  let user = registered_users.find(u=>u.sessionid==sessionid);
-  if(sessionid && user){
-    req.user = user;
-  }
-};
-
-const sendData = function(req,res){
-  res.setHeader("Content-Type","application/json");
-  let commentsInfo = JSON.stringify(comments);
-  res.write(commentsInfo);
-  res.end();
-}
 
 const getContentType = function(file) {
   let extn = file.slice(file.lastIndexOf("."));
@@ -97,49 +54,20 @@ const getContentType = function(file) {
   return extenstions[extn];
 };
 
-const addComments = function(req,res) {
-  let filePath = "data/comments.json";
-  let commentData = req.body;
-  if(commentData.name && commentData.comment){
-    commentData.time = new Date().toLocaleString();
-    comments.unshift(commentData);
-    let stringifiedComments = JSON.stringify(comments, null, 2);
-    fs.writeFileSync(filePath, stringifiedComments, "utf8");
-  }
-  res.redirect("/guest-page.html");
+const respondWith404 = function(req,res){
+  res.statusCode = 404;
+  res.write("not found");
   res.end();
-};
-
-const handleCookies = (req,res)=>{
-  let user = registered_users.find(u=>u.userName==req.body.userName);
-  if(!user) {
-    res.setHeader('Set-Cookie',`logInFailed=true`);
-    res.redirect('/guest-page.html');
-    return;
-  }
-  let sessionid = new Date().getTime();
-  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
-  user.sessionid = sessionid;
-  res.redirect('/guest-page.html');
-};
-
-const logoutUser = (req,res)=>{
-  res.setHeader('Set-Cookie',[`logInFailed=false;expires=${new Date(1).toString()}`,`sessionid=0;expires=${new Date(1).toString()}`]);
-  delete req.user.sessionid;
-  res.redirect('/guest-page.html');
-};
+}
 
 let app = WebApp.create();
 app.use(logRequest);
-app.use(loadUser);
 app.use(serveFile);
-app.get("/data",sendData);
-app.get("/guest-page.html",serveGuestPage);
-app.post("/submitComment",addComments);
-app.post("/login",handleCookies);
-app.get("/logout",logoutUser);
+app.use(respondWith404);
 
 const PORT = 5000;
 let server = http.createServer(app);
 server.on('error',e=>console.error('**error**',e.message));
 server.listen(PORT,(e)=>console.log(`server listening at ${PORT}`));
+
+module.exports = app;
