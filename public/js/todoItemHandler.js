a6ad1c58b0fdd4b0f2ced1843ee49aabb91e4952
ravinder <jajoriaravinder99@gmail.com>
2018-01-19 09:43:38 +0530

    Jayanth forced commit

--- public/js/todoItemHandler.js
+++ public/js/todoItemHandler.js
@@ -1,7 +1,17 @@
+const showItem = function(){
+  let itemText = this.responseText.text;
+  let todoId = this.responseText.todoId;
+  let itemId = this.responseText.itemId;
+}
+
 const createItem = function(){
   let para = document.createElement("p");
   let text = document.querySelector("#itemText").value;
   para.innerHTML = text;
   let itemsDiv = document.getElementsByClassName("items")[0];
   itemsDiv.appendChild(para);
+  let req = XMLHttpRequest();
+  req.addEventListener("load",showItem);
+  req.open("POST","/addItem");
+  req.send(`todoItem=${text}`);
 };
